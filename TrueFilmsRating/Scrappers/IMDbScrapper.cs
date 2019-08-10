61cdbfeb93f71f233a99a6a8f8cb95f8bf8a12b9
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-10 16:41:11 +0200

    Using one http client per thread

--- TrueFilmsRating/Scrappers/IMDbScrapper.cs
+++ TrueFilmsRating/Scrappers/IMDbScrapper.cs
@@ -10,28 +10,44 @@ using TrueFilmsRating.Models;
 
 namespace TrueFilmsRating.Scrappers
 {
-    public  class IMDbScrapper
+    public class IMDbScrapper : IDisposable
     {
         private readonly string _IMDbSearchUrl;
         private readonly int _numberOfThreads;
         private readonly string _APIKey;
 
-        public IMDbScrapper(string IMDbSearchUrl, int numberOfThreads,string APIKey)
+        List<FlurlClient> _flurlClients;
+
+        public IMDbScrapper(string IMDbSearchUrl, int numberOfThreads, string APIKey)
         {
             _IMDbSearchUrl = IMDbSearchUrl;
-            _numberOfThreads = numberOfThreads;
+            _numberOfThreads = numberOfThreads > 0 ? numberOfThreads : 1;
             _APIKey = APIKey;
+
+            _flurlClients = new List<FlurlClient>();
+            for (int i = 0; i < _numberOfThreads; i++)
+            {
+                _flurlClients.Add(new FlurlClient(_IMDbSearchUrl));
+            }
         }
 
-        public async Task<IMDbResponse> GetMovie(string title)
+        public async Task<IMDbResponse> GetMovie(string title, int flurlClientIndex = 0)
         {
-            // Flurl will use 1 HttpClient instance per host
-            return await _IMDbSearchUrl
+            return await _flurlClients[flurlClientIndex].Request()
                 .SetQueryParams(new { apikey = _APIKey, t = title })
                 .WithTimeout(10)
                 .GetJsonAsync<IMDbResponse>();
         }
+
+        public void Dispose()
+        {
+            foreach (var client in _flurlClients)
+            {
+                client.Dispose();
+            }
+        }
+
     }
 
-    
+
 }
