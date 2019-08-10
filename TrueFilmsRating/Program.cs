3ccb547bc9f816c4248c0570948fbde4fa591147
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-10 14:45:57 +0200

    Init

---
+++ TrueFilmsRating/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrueFilmsRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
