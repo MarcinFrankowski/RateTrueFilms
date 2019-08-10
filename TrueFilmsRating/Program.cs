0341f704e2efa16f255c3796d490afd8a64283b2
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-10 18:11:53 +0200

    Saving to csv and xslsx file

--- TrueFilmsRating/Program.cs
+++ TrueFilmsRating/Program.cs
@@ -1,9 +1,14 @@
-﻿using HtmlAgilityPack;
+﻿using CsvHelper;
+using HtmlAgilityPack;
 using Microsoft.Extensions.Configuration;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using TrueFilmsRating.Models;
 using TrueFilmsRating.Scrappers;
@@ -60,7 +65,13 @@ namespace TrueFilmsRating
                         DisplayMenu();
                         break;
                     case '2':
-                        SaveMovies(movies);
+                        SaveMoviesToCSV(movies);
+                        Console.Out.WriteLine($"{rn}Movies saved in truefilms.csv");
+                        DisplayMenu();
+                        break;
+                    case '3':
+                        SaveMoviesToXLSX(movies);
+                        Console.Out.WriteLine($"{rn}Movies saved in truefilms.xlsx");
                         DisplayMenu();
                         break;
                     case 'X':
@@ -74,12 +85,14 @@ namespace TrueFilmsRating
 
         }
 
+
         #region private methods
         private static void DisplayMenu()
         {
             Console.Out.Write($"{rn}What now? Choose an option by pressing a key:{rn}" +
                 $"1 - Display movies ordered by rating.{rn}" +
-                $"2 - Save movies data to movies.csv.{rn}" +
+                $"2 - Save movies data to CSV file (truefilms.csv).{rn}" +
+                $"3 - Save movies data to Excel file (truefilms.xlsx).{rn}" +
                 $"x - Close.{rn}");
         }
 
@@ -97,9 +110,51 @@ namespace TrueFilmsRating
             }
         }
 
-        private static void SaveMovies(IEnumerable<IMDbResponse> movies)
+        private static void SaveMoviesToCSV(IEnumerable<IMDbResponse> movies)
         {
-            throw new NotImplementedException();
+            using (var writer = new StreamWriter("truefilms.csv"))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(movies);
+            }
+        }
+
+        private static void SaveMoviesToXLSX(IEnumerable<IMDbResponse> movies)
+        {
+            List<string> colNames = new List<string>();
+            foreach (PropertyInfo prop in typeof(IMDbResponse).GetProperties())
+            {
+                colNames.Add(prop.Name);
+            }
+
+            using (FileStream stream = new FileStream("truefilms.xlsx", FileMode.Create))
+            {
+                IWorkbook wb = new XSSFWorkbook();
+                ISheet sheet = wb.CreateSheet("TrueFilms");
+                ICreationHelper cH = wb.GetCreationHelper();
+
+                IRow topRow = sheet.CreateRow(0);
+                int ic = 0;
+                foreach (var col in colNames)
+                {
+                    ICell cell = topRow.CreateCell(ic);
+                    cell.SetCellValue(col);
+                    ic++;
+                }
+
+                int im = 1;
+                foreach (var movie in movies)
+                {
+                    IRow row = sheet.CreateRow(im);
+                    for (int j = 0; j < colNames.Count; j++)
+                    {
+                        ICell cell = row.CreateCell(j);
+                        cell.SetCellValue(typeof(IMDbResponse).GetProperties().Single(p => string.Equals(p.Name, colNames[j])).GetValue(movie).ToString());
+                    }
+                    im++;
+                }
+                wb.Write(stream);
+            }
         }
 
         private static IConfigurationRoot GetConfig()
